27fba7d1afba7574b573c20e52986894e91ede7b
apple <stevenkitter@163.com>
2018-09-06 16:15:02 +0800

    log

--- auth/main.go
+++ auth/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	micro "github.com/micro/go-micro"
-	logger "github.com/sirupsen/logrus"
 	"github.com/stevenkitter/wxapi/db"
 	"github.com/stevenkitter/wxapi/global"
-	"github.com/stevenkitter/wxapi/log"
 	"github.com/stevenkitter/wxapi/micro/trace"
 	proto "github.com/stevenkitter/wxapi/proto/wxAuth"
 	"github.com/stevenkitter/wxapi/tracing"
@@ -19,25 +16,19 @@ const (
 	serverName = "wx.srv.auth"
 )
 
-var (
-	serverLogger *logger.Entry
-)
-
 func main() {
 	//mysql
 	mysql, err := db.InitMysql()
 	Migrate(mysql) //migrate
 	if err != nil {
-		serverLogger.Panic(err)
+		log.Panic(err)
 	}
 	defer mysql.Close()
-	//logger
-	serverLogger = log.InitLogger(serverName)
 	//tracing
 	tracer, closer, err := tracing.InitTracing(serverName)
 	defer closer.Close()
 	if err != nil {
-		serverLogger.Panic(err)
+		log.Panic(err)
 	}
 	service := micro.NewService(
 		micro.Name(serverName),
@@ -51,15 +42,8 @@ func main() {
 		db: mysql,
 	}
 	proto.RegisterWxAuthHandler(service.Server(), wxAuth)
-	fmt.Println("run the server")
-	serverLogger.Info("Run the server")
+	log.Println("Run the server")
 	if err := service.Run(); err != nil {
-		serverLogger.Panic(err)
+		log.Panicln(err)
 	}
-}
-
-func init() {
-	logger.SetFormatter(&logger.JSONFormatter{})
-	logger.SetOutput(os.Stdout)
-	logger.SetLevel(logger.InfoLevel)
 }
