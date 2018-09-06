f2d3556bc0150747dae0a74c2b1c7c3d2a7374c0
apple <stevenkitter@163.com>
2018-09-06 16:18:43 +0800

    main

--- auth/main.go
+++ auth/main.go
@@ -7,9 +7,7 @@ import (
 	micro "github.com/micro/go-micro"
 	"github.com/stevenkitter/wxapi/db"
 	"github.com/stevenkitter/wxapi/global"
-	"github.com/stevenkitter/wxapi/micro/trace"
 	proto "github.com/stevenkitter/wxapi/proto/wxAuth"
-	"github.com/stevenkitter/wxapi/tracing"
 )
 
 const (
@@ -24,18 +22,9 @@ func main() {
 		log.Panic(err)
 	}
 	defer mysql.Close()
-	//tracing
-	tracer, closer, err := tracing.InitTracing(serverName)
-	defer closer.Close()
-	if err != nil {
-		log.Panic(err)
-	}
 	service := micro.NewService(
 		micro.Name(serverName),
 		micro.Version(global.Version),
-		micro.WrapHandler(trace.NewHandlerWrapper(tracer)),
-		micro.WrapClient(trace.NewClientWrapper(tracer)),
-		micro.WrapSubscriber(trace.NewSubscriberWrapper(tracer)),
 	)
 	service.Init()
 	wxAuth := &WxAuth{
